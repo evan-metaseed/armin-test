e32d0d1c955ec0f1a6efd3ddfbcb6702c54c8448
evan <evan@metaseedlabs.com>
2022-11-25 11:36:41 +0200

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,90 @@
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+
+let secret = require("./secret");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.13",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.7.0",
+      },
+      {
+        version: "0.6.6",
+      },
+      {
+        version: "0.4.24",
+      },
+    ],
+  },
+  networks: {
+    rinkeby: {
+      url: secret.rinkebyURL,
+      accounts: [secret.key],
+    },
+    ropsten: {
+      url: secret.ropstenURL,
+      accounts: [secret.key],
+    },
+    mainnet: {
+      url: secret.url,
+      accounts: [secret.key],
+    },
+    goerli: {
+      url: secret.goerliURL,
+      accounts: [secret.key],
+    },
+  }
+};
---
+++ test/armin-tests.js
@@ -0,0 +1,167 @@
+const hre = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+const fs = require('fs');
+
+
+describe("Armin Test", function () {
+
+  let Contract;
+  let contract;
+
+  let preRoot;
+  let preMerkle;
+
+  let owner;
+  let addrs; // address
+
+  beforeEach(async function () {
+    [owner, ...addrs] = await ethers.getSigners();
+    
+    let whitelist = fs.readFileSync('./whitelist/localtest.json');
+    whitelist = JSON.parse(whitelist);
+
+    const preList = whitelist.pre.map(address => keccak256(ethers.utils.getAddress(address)));
+
+    preMerkle = new MerkleTree(preList, keccak256, {sortPairs : true});
+    preRoot = preMerkle.getHexRoot();
+
+    const name = "Test"
+    const symbol = "TESTSYMBOL"
+
+    Contract = await ethers.getContractFactory("ArminAAA");
+    contract = await Contract.deploy(
+      name, 
+      symbol
+    );
+    
+    // set merkleRoot
+    await contract.setAllowlistRoot(preRoot);
+
+  })
+
+  describe("Test internalMint()", function () {
+    it("test max supply", async function () {
+
+      // supplies 
+      let maxSupply = await contract.MAX_SUPPLY()
+
+      // mint too many internal tokens -- 
+      await expect(contract.internalMint(maxSupply + 1, owner.address)).to.revertedWith("would exceed max supply")
+
+    });
+  })
+
+  describe("Test allowlistMint()", function () {
+
+    let allowlistPrice;
+    let pubPrice;
+    let maxAllowlist;
+    
+    beforeEach(async function () {
+      allowlistPrice = ethers.utils.parseEther("0.15")
+      pubPrice = ethers.utils.parseEther("0.15")
+      // max allowed to mint per wallet on allowlist
+      maxAllowlist = await contract.maxAllowlist();
+      maxSupply = await contract.MAX_SUPPLY()
+    })
+
+    it("Test Allowlist Mints", async function () {
+      // set allowlist to true
+      await contract.setAllowlistActive(true);
+      const proof = preMerkle.getHexProof(keccak256(owner.address));
+
+      // mint maxAllowlist
+      await contract.allowlistMint(owner.address, maxAllowlist, proof, {value: allowlistPrice.mul(maxAllowlist)})
+
+      // expect total supply to increase by maxAllowlist
+      let totalSupply = await contract.totalSupply();
+      expect(totalSupply).to.equal(maxAllowlist);
+
+      // try minting 1 more than maxAllowlist
+      await expect(contract.allowlistMint(owner.address, 1, proof, {value: allowlistPrice})).to.revertedWith("Exceeded max available to purchase");
+      
+      // totalSupply should remain the same
+      expect(totalSupply).to.equal(maxAllowlist);
+    })
+
+    it("Test Ether value sent is correct", async function () {
+      // set sale switches to true
+      await contract.setPublicActive(true);
+      await contract.setAllowlistActive(true);
+
+      const proof = preMerkle.getHexProof(keccak256(owner.address));
+
+      // try sending less than tokenPrice (should fail)
+      const preLowPrice = allowlistPrice.sub(ethers.utils.parseEther("0.01"));
+      const pubLowPrice = pubPrice.sub(ethers.utils.parseEther("0.01"));
+
+      // presale
+      await expect(contract.allowlistMint(owner.address, 1, proof, {value: preLowPrice})).to.revertedWith("Incorrect funds");
+   
+      // pubsale
+      await expect(contract.publicMint(owner.address, 1, {value: pubLowPrice})).to.revertedWith("Incorrect funds");
+
+      // try sending exactly tokenPrice (no problem)
+      await expect(await contract.allowlistMint(owner.address, 1, proof, {value: allowlistPrice})).to.changeEtherBalance(owner, new ethers.BigNumber.from("0").sub(allowlistPrice))
+      expect(await contract.totalSupply()).to.equal(1);
+    })
+
+    it("Test _allowlistCounter is updated and functioning", async function () {
+      // set presale to true
+      await contract.setAllowlistActive(true);
+
+      // get proof
+      const proof = preMerkle.getHexProof(keccak256(owner.address));
+
+      // ensure balance and presale counter are correct, by minting maxAllowlist
+      await contract.allowlistMint(owner.address, maxAllowlist, proof, {value: allowlistPrice.mul(maxAllowlist)});
+      expect(await contract.balanceOf(owner.address)).to.equal(maxAllowlist);
+      expect(await contract._allowlistCounter(owner.address)).to.equal(maxAllowlist);
+
+      // attempt to mint more than counter allows (maxAllowlist)
+      await expect(contract.allowlistMint(owner.address, 1, proof, {value: allowlistPrice})).to.revertedWith('Exceeded max available to purchase');
+      
+      // ensure balance and presale counter are correct
+      expect(await contract._allowlistCounter(owner.address)).to.equal(maxAllowlist);
+      expect(await contract.balanceOf(owner.address)).to.equal(maxAllowlist);
+    })
+
+    it("test merkle", async function() {
+      // set presale to true
+      await contract.setAllowlistActive(true);
+
+      // assign address not in allowlist
+      const wrongAddress = '0xf8817128624eA0Ca15400dEE922D81121c9B9839';
+      // get proof
+      const proof = preMerkle.getHexProof(keccak256(wrongAddress));
+
+      // mint with address that isn't on the allowlist
+      await expect(contract.allowlistMint(wrongAddress, 1, proof, {value: allowlistPrice})).to.revertedWith('Invalid MerkleProof');
+    });
+  });
+
+  describe("Test totalSupply", function () {
+    it("test if totalSupply changes if token is tranferred to 0 address", async function () {
+      // set public sale to true
+      await contract.setPublicActive(true);
+
+      let pubPrice = await contract.PRICE();
+
+      // check totalSupply
+      expect(await contract.totalSupply()).to.equal(0);
+
+      // mint a token
+      await contract.publicMint(owner.address, 1, {value: pubPrice});
+      
+      // check supply
+      expect(await contract.totalSupply()).to.equal(1);
+
+      // transfer token to 0 address
+      await expect(contract.transferFrom(owner.address, ethers.constants.AddressZero, 0)).to.be.revertedWith("TransferToZeroAddress()");
+
+    });
+  })
+});
